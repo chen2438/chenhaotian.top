5f6b67c6fe67b040960d8ab598890e9c8691a9b4
陈昊天 <2363822647@qq.com>
2023-05-22 15:36:41 +0800

    Update rebuild.sh

--- source/rebuild.sh
+++ source/rebuild.sh
@@ -1,4 +1,7 @@
 cd /var/www/html
-rm -rf public public.zip
-wget https://storage.opennet.top/directlink/local/public.zip
-unzip public.zip
+rm -rf chenhaotian.top
+git clone https://gitee.com/chen2438/chenhaotian.top.git
+cd chenhaotian.top
+npm install
+hexo clean
+hexo generate
