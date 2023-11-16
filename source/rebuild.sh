0474394e7caf02106e98f61939fe7788e73d3e5f
chen2438 <2363822647@qq.com>
2023-11-16 11:30:56 +0800

    upd

--- source/rebuild.sh
+++ source/rebuild.sh
@@ -1,6 +1,6 @@
 cd /var/www/html
 rm -rf chenhaotian.top
-git clone https://ghproxy.com/https://github.com/chen2438/chenhaotian.top.git
+git clone https://mirror.ghproxy.com/https://github.com/chen2438/chenhaotian.top.git
 cd chenhaotian.top
 npm install
 hexo clean
