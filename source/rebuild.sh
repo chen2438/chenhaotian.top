c25032511f919e016ce295afaba9f75f95e7f43a
陈昊天 <2363822647@qq.com>
2024-01-21 20:51:37 +0800

    Update rebuild.sh

--- source/rebuild.sh
+++ source/rebuild.sh
@@ -1,7 +1,18 @@
 cd /var/www/html
-rm -rf chenhaotian.top
-git clone https://mirror.ghproxy.com/https://github.com/chen2438/chenhaotian.top.git
-cd chenhaotian.top
-npm install
-hexo clean
+rm -rf blog-repo
+
+# git clone https://mirror.ghproxy.com/https://github.com/chen2438/chenhaotian.top.git blog-repo
+git clone https://mirror.vayki.com/proxy/https://github.com/chen2438/chenhaotian.top.git blog-repo
+
+cd blog-repo
+
+# npm cache clean --force
+# rm -rf node_modules/
+npm install --registry=https://registry.npmmirror.com/
+
+# hexo clean
 hexo generate
+
+rm -rf ../chenhaotian.top/
+mkdir -p ../chenhaotian.top/piblic/
+cp -r public/ ../chenhaotian.top/public/
